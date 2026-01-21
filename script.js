13b82810c6fab4f2299beceb81290971fa2209ce
hancoooi <ekhanci1907@gmail.com>
2026-01-21 11:40:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const days = {
-  "1 Temmuz": "1 Temmuz 1937’de MacGregor Arktik Seferi başladı.
-Sefer, Kanada’nın Ellesmere Adası’ndaki Fort Conger bölgesine ulaşarak Kuzey Kutbu çevresinde meteoroloji, jeomanyetizma ve kutup ışıkları üzerine bilimsel gözlemler yapmak amacıyla düzenlendi.Araştırma gemisi Donald II ile yürütülen çalışmalar, zorlu kış koşulları da dâhil olmak üzere 1938 sonbaharına kadar devam etti.Bu sefer, Kuzey Kutbu’nda modern ve sistematik bilimsel ölçümlerin uygulanması açısından önemlidir.Özellikle hava durumu, manyetik alan ve aurora gözlemleri, Arktik bölgenin fiziksel koşullarının daha iyi anlaşılmasına katkı sağlamış ve sonraki kutup araştırmalarına bilimsel altyapı oluşturmuştur.
-    Türkiye’nin Ulusal Arktik Bilimsel Araştırma Seferi(TASE), bu tarihte Arktik Okyanusu’nda bilimsel ölçümler gerçekleştirmiştir.",
+  "7 Temmuz": "Türkiye’nin Ulusal Arktik Bilimsel Araştırma Seferi (TASE), bu tarihte Arktik Okyanusu’nda bilimsel ölçümler gerçekleştirmiştir.",
   "15 Temmuz": "Kutup bölgelerinde deniz buzu kalınlığı ve sıcaklık değişimleri üzerine gözlemler yapılmıştır.",
   "1 Ağustos": "Kutup ekosistemlerinde iklim değişikliğinin etkileri bilimsel raporlarla ortaya konmuştur.",
   "6 Ağustos": "Arktik seferleri kapsamında deniz suyu ve atmosfer örnekleri toplanmıştır."
